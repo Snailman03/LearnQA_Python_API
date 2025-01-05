116ae97acc2f022f33352fc6c3f7041aa31d1ec7
Ivan_Mitin <is-mitin@yandex.ru>
2025-01-05 19:31:43 +0300

    Task_13_решение2_корявое

--- Lesson_3_Tasks/task_13.py
+++ Lesson_3_Tasks/task_13.py
@@ -9,6 +9,7 @@ class TestThirteenTask:
         ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.77 Safari/537.36 Edg/91.0.100.0'),
         ('Mozilla/5.0 (iPad; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1')
     ]
+    list_incorrect_parse_ua = []
 
     @pytest.mark.parametrize('user_agent', user_agents)
     def test_user_agent_parse(self, user_agent):
@@ -18,7 +19,7 @@ class TestThirteenTask:
         response = requests.get(url, headers={"User-Agent": user_agent})
         response_dict = response.json()
         assert response.status_code == 200
-        list_incorrect_parse_ua = []
+
 
         if user_agent == ('Mozilla/5.0 (Linux; U; Android 4.0.2; en-us; Galaxy Nexus Build/ICL53F) '
                           'AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'):
@@ -77,7 +78,7 @@ class TestThirteenTask:
             except AssertionError:
                 list_incorrect_parse_ua.append(user_agent)
 
-        print(list_incorrect_parse_ua)
+    print(list_incorrect_parse_ua)
 
 
 
