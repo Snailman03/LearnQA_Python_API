e465b82b3d3b96c45ae93af095a0b479a6fd16c0
Ivan_Mitin <is-mitin@yandex.ru>
2025-01-17 11:52:26 +0300

    ADD switch environment, but doesn't work properly

--- lib/my_requests.py
+++ lib/my_requests.py
@@ -1,38 +1,39 @@
 import requests
 import allure
 from lib.logger import Logger
+from environment import ENV_OBJECT
 
 
 class MyRequests():
 
     @staticmethod
-    def post (url:str, data:dict = None,
-              headers:dict = None, cookies:dict = None):
+    def post(url: str, data: dict = None,
+             headers: dict = None, cookies: dict = None):
         with allure.step(f"POST requests to URL {url}"):
             return MyRequests._send(url, data, headers, cookies, 'POST')
 
     @staticmethod
     def get(url: str, data: dict = None,
-             headers: dict = None, cookies: dict = None):
+            headers: dict = None, cookies: dict = None):
         with allure.step(f"GET requests to URL {url}"):
             return MyRequests._send(url, data, headers, cookies, 'GET')
 
     @staticmethod
     def put(url: str, data: dict = None,
-             headers: dict = None, cookies: dict = None):
+            headers: dict = None, cookies: dict = None):
         with allure.step(f"PUT requests to URL {url}"):
             return MyRequests._send(url, data, headers, cookies, 'PUT')
 
     @staticmethod
     def delete(url: str, data: dict = None,
-             headers: dict = None, cookies: dict = None):
+               headers: dict = None, cookies: dict = None):
         with allure.step(f"DELETE requests to URL {url}"):
             return MyRequests._send(url, data, headers, cookies, 'DELETE')
 
     @staticmethod
-    def _send(url:str, data:dict, headers:dict, cookies:dict,method: str):
+    def _send(url: str, data: dict, headers: dict, cookies: dict, method: str):
 
-        url = f"https://playground.learnqa.ru/api{url}"
+        url = f"{ENV_OBJECT.get_base_url()}{url}"
 
         if headers is None:
             headers = {}
@@ -42,17 +43,17 @@ class MyRequests():
         Logger.add_request(url, data, headers, cookies, method)
 
         if method == 'GET':
-            response = requests.get(url, params = data, headers = headers,
-                                    cookies = cookies)
+            response = requests.get(url, params=data, headers=headers,
+                                    cookies=cookies)
         elif method == 'POST':
-            response = requests.post(url, data = data, headers = headers,
-                                    cookies=cookies)
+            response = requests.post(url, data=data, headers=headers,
+                                     cookies=cookies)
         elif method == 'PUT':
-            response = requests.put(url, data = data, headers = headers,
-                                cookies=cookies)
-        elif method == 'DELETE':
-            response = requests.delete(url, data = data, headers = headers,
+            response = requests.put(url, data=data, headers=headers,
                                     cookies=cookies)
+        elif method == 'DELETE':
+            response = requests.delete(url, data=data, headers=headers,
+                                       cookies=cookies)
         else:
             raise Exception(f"Bad http method '{method}' was received ")
 
